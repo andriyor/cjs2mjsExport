88d9b667a3d125cdb43506e1d2551bce38249303
Andrii Oriekhov <andriyorehov@gmail.com>
2024-01-06 15:59:48 +0200

    add new case

---
+++ test/test-project-expected/case3/usage.ts
@@ -0,0 +1,4 @@
+import { sum, subtract } from './sum';
+
+console.log(sum(2, 8));
+console.log(subtract(8, 2));
---
+++ test/test-project/case3/sum.ts
@@ -0,0 +1,10 @@
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number) => {
+  return a - b;
+};
+
+module.exports.sum = sum;
+module.exports.subtract = subtract;
