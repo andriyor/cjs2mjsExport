330bef60978c2eab062fae3d9741d5d716223bfd
Andrii Oriekhov <andriyorehov@gmail.com>
2024-01-06 23:38:46 +0200

    add new test case with multiple usages

---
+++ test/test-project-expected/case9ManyUsages/usage.ts
@@ -0,0 +1,3 @@
+import { sumTool } from './sum';
+
+console.log(sumTool(2, 8));
---
+++ test/test-project/case9ManyUsages/sum.ts
@@ -0,0 +1,5 @@
+const sumTool = (a: number, b: number) => {
+  return a + b;
+};
+
+module.exports.sumTool = sumTool;
